a2202da2916e9e7dd8eeabd7f3f1b40bef47d92f
najongjine <najongjin3@hotmail.com>
2025-11-12 11:47:03 +0900

    .

--- test2.ts
+++ test2.ts
@@ -1,8 +1,9 @@
-// npm i -g typescript
-// npm i -g tsx
-// tsx test2.ts
-console.log("타입스크립트에요");
-// let : 변수를 선언하다
-let a: number = 1;
-let b: string = `2`; // '', "", ``
-console.log(`a 는 :${a}, b 는 ${b}`);
+// // npm i -g typescript
+// // npm i -g tsx
+// // tsx test2.ts
+// console.log("타입스크립트에요");
+// // let : 변수를 선언하다
+// let a: number = 1;
+// let b: string = `2`; // '', "", ``
+// console.log(`a 는 :${a}, b 는 ${b}`);
+// console.dir("dfdsd");
