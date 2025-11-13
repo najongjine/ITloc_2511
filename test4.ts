5286f50a74ae708d90d027c1886f843ac5bbbe6a
najongjine <najongjin3@hotmail.com>
2025-11-13 09:18:32 +0900

    .

---
+++ test4.ts
@@ -0,0 +1,14 @@
+let a = false;
+let b = 0;
+
+let c = true;
+let d = 9999;
+
+let str1 = "sss";
+
+/* 어려운 자료형 */
+let array1 = [3, 1, 9];
+let jobject1 = { name: "홍길동", age: 20 };
+
+console.log(`a:${a}`);
+console.log(`str1:${str1}`);
