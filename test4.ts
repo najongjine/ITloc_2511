c8c503176daeba389929614768365f9b88b6e97f
najongjine <najongjin3@hotmail.com>
2025-11-13 09:31:33 +0900

    .

--- test4.ts
+++ test4.ts
@@ -10,7 +10,4 @@ let str1 = "sss";
 let array1 = [3, 1, 9];
 let jobject1 = { name: "홍길동", age: 20 };
 
-console.log(`a:${a}`);
-console.log(`str1:${str1}`);
-console.log(`array1: `, array1);
-console.log(`jobject1: `, jobject1);
+console.log(`array1[0] : ${array1[0]}`);
