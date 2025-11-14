2b43996f37929c122355e7ea2214166cd3efa793
najongjine <najongjin3@hotmail.com>
2025-11-14 09:32:41 +0900

    .

--- test5.ts
+++ test5.ts
@@ -23,6 +23,28 @@ if (a <= b) {
 a = 1;
 b = 2;
 
-if (a >= b && b >= a) {
+if (1 >= 2 && 2 >= 1) {
   console.log(`a>=b && b>=a 통과 될까요?`);
+}
+
+let username = "홍길동";
+let password = "1234";
+if (!username || username != `홍길동` || !password || password != `1234`) {
+  console.log(`이거 출력 될까요?`);
+}
+
+let score = 75;
+if (score >= 90) {
+  console.log(`A 학점 입니다`);
+} else if (score >= 80) {
+  console.log(`B 학점 입니다`);
+} else {
+  console.log(` C~F 학점 이겠죠`);
+}
+
+
+score =60;
+
+else if( score == 60){
+    console.log(`점수가 60이랑 같네요`);
 }
