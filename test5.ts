4874e52087e508178e329674bf87aba409ba916f
najongjine <najongjin3@hotmail.com>
2025-11-14 10:09:38 +0900

    .

--- test5.ts
+++ test5.ts
@@ -33,18 +33,23 @@ if (!username || username != `홍길동` || !password || password != `1234`) {
   console.log(`이거 출력 될까요?`);
 }
 
-let score = 75;
+/* 학점 분류기를 만들어 주세요
+A - 100~90점 
+B - 89~80
+C - 79~70
+D - 69~60
+F - 60 미만
+*/
+let score = 90;
+
 if (score >= 90) {
-  console.log(`A 학점 입니다`);
-} else if (score >= 80) {
-  console.log(`B 학점 입니다`);
+  console.log(`A 학점`);
+} else if (score <= 89 && score >= 80) {
+  console.log(`B 학점`);
+} else if (score <= 79 && score >= 70) {
+  console.log(`C 학점`);
+} else if (score <= 69 && score >= 60) {
+  console.log(`D 학점`);
 } else {
-  console.log(` C~F 학점 이겠죠`);
-}
-
-
-score =60;
-
-else if( score == 60){
-    console.log(`점수가 60이랑 같네요`);
+  console.log(`F 학점`);
 }
