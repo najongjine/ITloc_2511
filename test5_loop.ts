622df3fcedd4d88bf9a764bf34982daefad07381
najongjine <najongjin3@hotmail.com>
2025-11-14 11:07:05 +0900

    .

--- test5_loop.ts
+++ test5_loop.ts
@@ -1,8 +1,9 @@
 /* 반복문 */
 // i= i+1
 //let i = 0;
-for (let i = 0; i < 3; i++) {
-  console.log(` i: ${i}`);
-}
 
-//
+for (let i = 0; i <= 1; i++) {
+  for (let k = 0; k <= 1; k++) {
+    console.log(`i: ${i}, k: ${k}`);
+  }
+}
