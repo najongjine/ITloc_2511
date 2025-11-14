7794501de7365c0ad0ca3f71089e20a9dc3ed0fa
najongjine <najongjin3@hotmail.com>
2025-11-14 10:34:20 +0900

    .

--- test5_if.ts
+++ test5_if.ts
@@ -1,76 +1,76 @@
-/* if 문 연습 */
-if (true && false && true && true) {
-  console.log(` && 연산 결과에요`);
-}
+// /* if 문 연습 */
+// if (true && false && true && true) {
+//   console.log(` && 연산 결과에요`);
+// }
 
-if (false || false || false || false || false) {
-  console.log(` OR 연산 결과에요`);
-}
+// if (false || false || false || false || false) {
+//   console.log(` OR 연산 결과에요`);
+// }
 
-let a = 1;
-let b = 2;
+// let a = 1;
+// let b = 2;
 
-if (a > b) {
-  console.log(``);
-}
-if (a >= b) {
-  console.log(`a 는 b보다 크거나 같습니다`);
-}
-if (a <= b) {
-  console.log(`a 는 b 보다 같거나 작습니다`);
-}
+// if (a > b) {
+//   console.log(``);
+// }
+// if (a >= b) {
+//   console.log(`a 는 b보다 크거나 같습니다`);
+// }
+// if (a <= b) {
+//   console.log(`a 는 b 보다 같거나 작습니다`);
+// }
 
-a = 1;
-b = 2;
+// a = 1;
+// b = 2;
 
-if (1 >= 2 && 2 >= 1) {
-  console.log(`a>=b && b>=a 통과 될까요?`);
-}
+// if (1 >= 2 && 2 >= 1) {
+//   console.log(`a>=b && b>=a 통과 될까요?`);
+// }
 
-let username = "홍길동";
-let password = "1234";
-if (!username || username != `홍길동` || !password || password != `1234`) {
-  console.log(`이거 출력 될까요?`);
-}
+// let username = "홍길동";
+// let password = "1234";
+// if (!username || username != `홍길동` || !password || password != `1234`) {
+//   console.log(`이거 출력 될까요?`);
+// }
 
-/* 학점 분류기를 만들어 주세요
-A - 100~90점 
-B - 89~80
-C - 79~70
-D - 69~60
-F - 60 미만
-*/
+// /* 학점 분류기를 만들어 주세요
+// A - 100~90점
+// B - 89~80
+// C - 79~70
+// D - 69~60
+// F - 60 미만
+// */
 
+// // if (score >= 90) {
+// //   console.log(`A 학점`);
+// // } else if (score <= 89 && score >= 80) {
+// //   console.log(`B 학점`);
+// // } else if (score <= 79 && score >= 70) {
+// //   console.log(`C 학점`);
+// // } else if (score <= 69 && score >= 60) {
+// //   console.log(`D 학점`);
+// // } else {
+// //   console.log(`F 학점`);
+// // }
+// let score = 71;
 // if (score >= 90) {
 //   console.log(`A 학점`);
-// } else if (score <= 89 && score >= 80) {
+// } else if (score >= 80) {
 //   console.log(`B 학점`);
-// } else if (score <= 79 && score >= 70) {
+// } else if (score >= 70) {
 //   console.log(`C 학점`);
-// } else if (score <= 69 && score >= 60) {
+// } else if (score >= 60) {
 //   console.log(`D 학점`);
 // } else {
 //   console.log(`F 학점`);
 // }
-let score = 71;
-if (score >= 90) {
-  console.log(`A 학점`);
-} else if (score >= 80) {
-  console.log(`B 학점`);
-} else if (score >= 70) {
-  console.log(`C 학점`);
-} else if (score >= 60) {
-  console.log(`D 학점`);
-} else {
-  console.log(`F 학점`);
-}
 
-// if (true) {
-// }
+// // if (true) {
+// // }
 
-// if(true){}
-// else{}
+// // if(true){}
+// // else{}
 
-// if(){}
-// else if(){}
-// else {}
+// // if(){}
+// // else if(){}
+// // else {}
--- test5_loop.ts
+++ test5_loop.ts
@@ -1,5 +1,8 @@
 /* 반복문 */
 // i= i+1
+//let i = 0;
 for (let i = 0; i < 3; i++) {
   console.log(` i: ${i}`);
 }
+
+//
