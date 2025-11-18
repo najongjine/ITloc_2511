813e1d37a2e8679211dbfeb5afeaa2dfe7e8c89e
najongjine <najongjin3@hotmail.com>
2025-11-18 09:11:38 +0900

    .

--- test6_practice.ts
+++ test6_practice.ts
@@ -1,25 +1,25 @@
-/* 
-1~20 까지에서, 소수를 primes 배열에 저장하고
-출력해 보세요
-2 % 2 == 0,  2 % 3 = 2
- */
-let num = 20;
-let primes: number[] = [];
-for (let i = 0; i <= num; i++) {
-  let bPrime = true;
-  // i=4
-  let j = 2;
-  // 2<4
-  for (; j < i; j++) {
-    // 4 % 2 == 0
-    if (i % j == 0) {
-      // 발동
-      bPrime = false;
-    }
-  }
-  if (bPrime) {
-    false;
-    primes.push(i);
-  }
-}
-console.log(primes);
+// /*
+// 1~20 까지에서, 소수를 primes 배열에 저장하고
+// 출력해 보세요
+// 2 % 2 == 0,  2 % 3 = 2
+//  */
+// let num = 20;
+// let primes: number[] = [];
+// for (let i = 0; i <= num; i++) {
+//   let bPrime = true;
+//   // i=4
+//   let j = 2;
+//   // 2<4
+//   for (; j < i; j++) {
+//     // 4 % 2 == 0
+//     if (i % j == 0) {
+//       // 발동
+//       bPrime = false;
+//     }
+//   }
+//   if (bPrime) {
+//     false;
+//     primes.push(i);
+//   }
+// }
+// console.log(primes);
